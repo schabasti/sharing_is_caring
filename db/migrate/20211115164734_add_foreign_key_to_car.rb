1d06ce36de2a7d1ed6f1dc695aa1d6a9cdb558e0
Marcus von Wrede <m.vonwrede@gmx.de>
2021-11-16 13:04:13 +0100

    Changed migrations and done with seeding

--- db/migrate/20211115164734_add_foreign_key_to_car.rb
+++
@@ -1,5 +0,0 @@
-class AddForeignKeyToCar < ActiveRecord::Migration[6.0]
-  def change
-    add_foreign_key :cars, :users
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_103721) do
+ActiveRecord::Schema.define(version: 2021_11_15_164733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(version: 2021_11_16_103721) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
-    t.string "photo"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
   create_table "rentings", force: :cascade do |t|
