5cc95b692395bede96b0b72a52c77ac0b0ef81f6
Marcus von Wrede <m.vonwrede@gmx.de>
2021-11-16 14:34:32 +0100

    first changes

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,20 @@
 class CarsController < ApplicationController
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    if @car.save
+      redirect_to car_path(@car)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit()#:so....)
+  end
 end
