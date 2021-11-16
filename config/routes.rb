75b21588e065531442344a9dd803ac72686ffcb6
Marcus von Wrede <m.vonwrede@gmx.de>
2021-11-16 15:57:17 +0100

    create a new renting for car

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :cars, only: [:index]
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cars, only: [:index] do
+    resources :rentings, only: %i[new create]
+  end
 end
